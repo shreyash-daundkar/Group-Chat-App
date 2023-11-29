6c2bea4cf01920e9e87e6598730c45c282fa5129
Shreyash Daundkar <productiveappa@gmail.com>
2023-11-29 21:10:33 +0530

    Added chat service

---
+++ services/chat.js
@@ -0,0 +1,31 @@
+const Chat = require('../models/chat');
+const User = require('../models/user');
+
+exports.getChats = async options => {
+    try {
+
+        const chats = await Chat.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['id', 'username'],
+                },
+            ],
+        });
+
+        return chats;
+    } catch (error) {
+        console.error(error.stack);
+        throw error;
+    }
+}
+
+exports.addChat = async chat => {
+    try {
+        const res = await Chat.create(chat);
+        return res;
+    } catch (error) {
+        console.error(error.stack);
+        throw error;
+    }
+}
