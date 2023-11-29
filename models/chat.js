c1b23c9440f48d6ba4e59175c5d2ce83cd6dc086
Shreyash Daundkar <productiveappa@gmail.com>
2023-11-29 21:10:18 +0530

    Added chat model

---
+++ models/chat.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const database = require('../utils/database');
+
+module.exports = database.define('chat', {
+    id : {
+        type : Sequelize.INTEGER,
+        primaryKey : true,
+        allowNull : false,
+        autoIncrement : true,
+    },
+    message : {
+        type : Sequelize.STRING,
+        allowNull : false,
+    },
+});
