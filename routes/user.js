d3463741ba2347b9356008512687592c889f29c1
Shreyash Daundkar <productiveappa@gmail.com>
2023-12-02 20:36:33 +0530

    Added route for get users

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
-const { addUser, verifyUser } = require('../controllers/user');
+const { addUser, verifyUser, getUsers } = require('../controllers/user');
 
 const { signupValidation, loginValidation } = require('../middlewares/userValidation');
+const { userAuth } = require('../middlewares/userAuthentication');
 
 
 
@@ -10,8 +11,12 @@ const router = express.Router();
 
 
 
+router.get('/', userAuth, getUsers);
+
 router.post('/signup', signupValidation, addUser);
 
 router.post('/login',loginValidation, verifyUser);
+
+
 
 module.exports = router;
