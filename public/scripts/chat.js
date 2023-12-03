72abd8fc216bcb28ae4dc699aed1de7a8ec134fc
Shreyash Daundkar <productiveappa@gmail.com>
2023-12-03 06:19:37 +0530

    Changed frontend script to send and get chat by group id

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,8 +1,14 @@
 const host = 'http://localhost:4000';
 
+
+
+
 const chatBox = document.querySelector('.chat-box');
+const groupList = document.querySelector('#group-list');
 const sendChatBtn = document.querySelector('#send-chat-btn');
 const chatInput = document.querySelector('#chat-input');
+
+
 
 
 // authenticate
@@ -17,56 +23,60 @@ function getCookie(name) {
   }
   
 const token = getCookie('token');
-axios.defaults.headers.common['authorization'] = token;
+if (token) {
+    axios.defaults.headers.common['authorization'] = token;
+} else {
+    window.location.href = 'login.html';
+}
+
 
 
 
 //get chats
 
-window.addEventListener('DOMContentLoaded', loadChats);
-async function loadChats() { 
+let selectedGroupId;
+
+groupList.addEventListener('click', loadChats);
+async function loadChats(e) { 
+
+    if(!e.target.classList.contains('list-group-item')) return;
+
+    const groupItems = document.querySelectorAll('#group-list .list-group-item');
+    groupItems.forEach(item => item.classList.remove('active'));
+
+    const selectedGroup = e.target;
+    selectedGroup.classList.add('active');
+
+    selectedGroupId = selectedGroup.getAttribute('groupid');
     
     let lastMsgId = 0;
-    localStorage.setItem('global-chats', JSON.stringify([]));
+
+    localStorage.setItem('chats', JSON.stringify([]));
+
     setInterval(async () => {
-        const { data: { data }} = await axios.get(host+ `/chat?lastMsgId=${lastMsgId}`);
+
+        const { data: { data }} = await axios.get(host+ `/chat?groupId=${selectedGroupId}&lastMsgId=${lastMsgId}`);
         storeToLocalStorage(data);
-        const chats = JSON.parse(localStorage.getItem('global-chats'));
-        lastMsgId = chats[ chats.length - 1 ].id;
+
+        const chats = JSON.parse(localStorage.getItem('chats'));
+        if(chats.length !== 0) {
+            lastMsgId = chats[ chats.length - 1 ].id;
+        }
+
         chatBox.innerHTML = '';
         chats.forEach(chat => addChat(chat));
-    }, 1000);
+
+    }, 3000);
 }
 
 function storeToLocalStorage(data) {
-    const chats = JSON.parse(localStorage.getItem('global-chats'));
+    const chats = JSON.parse(localStorage.getItem('chats'));
     while(data.length !== 0) {
         if(chats.length >= 10) chats.shift();
         chats.push(data.shift());
     }
-    localStorage.setItem('global-chats', JSON.stringify(chats));
+    localStorage.setItem('chats', JSON.stringify(chats));
 }
-
- 
-
-//send chat 
-
-sendChatBtn.addEventListener('click', async e => {
-    e.preventDefault();
-    const message = chatInput.value;
-    if(message) {
-        try {
-            const res = await axios.post(host + '/chat', { message });
-            if(res.data.success) {
-                addChat({ username: 'You', message, isCurrUser: true });
-            }
-        } catch (error) {
-            console.log(error.response.data.message);
-        }
-    }
-});
-
-// utility 
 
 function addChat(chat) {
     let { username , message, isCurrUser } = chat;
@@ -81,3 +91,20 @@ function addChat(chat) {
     div.innerHTML = `<p><strong>${username}: </strong>${message}</p>`
     chatBox.append(div);
 }
+ 
+
+
+
+//send chat 
+
+sendChatBtn.addEventListener('click', async e => {
+    e.preventDefault();
+    const message = chatInput.value;
+    if(message) {
+        try {
+            const res = await axios.post(host + `/chat?groupId=${selectedGroupId}`, { message });
+        } catch (error) {
+            console.log(error.response.data.message);
+        }
+    }
+});
