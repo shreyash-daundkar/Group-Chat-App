7cdb4744cee1a344413d8db084e53f58b0a87920
Shreyash Daundkar <productiveappa@gmail.com>
2023-12-10 14:10:33 +0530

    Changed archived chat service

--- services/archivedChat.js
+++ services/archivedChat.js
@@ -1,14 +1,34 @@
+const database = require('../utils/database');
+
 const ArchivedChat = require('../models/archivedChat');
+const Chat = require('../models/chat');
 
-exports.saveToArchivedChat = async options => {
-    try {
-        const { chats } = options;
 
-        await ArchivedChat.bulkCreate(chats.map(chat => chat.toJSON()));
 
-        return;
-    } catch (error) {
-        console.error(error.stack);
-        throw error;
-    }
+exports.movedOldChatToArichived = async () => {
+    let t;
+  try {
+    t = await database.transaction();
+     
+    const chats = await Chat.findAll();
+
+    await ArchivedChat.bulkCreate(chats.map(chat => chat.toJSON()), {transaction: t});
+
+    const ChatId = chats.map(chat => chat.id);
+    await Chat.destroy({
+        where: {
+            id: ChatId,
+        },
+        transaction: t,
+    });
+
+    await t.commit();
+    return;
+      
+  } catch (error) {
+
+    await t.rollback();
+    console.error(error.stack);
+    throw error;
+  }
 }
