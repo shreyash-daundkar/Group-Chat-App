dc49cc836b65de336a2fc5af28f616e75bab3932
Shreyash Daundkar <productiveappa@gmail.com>
2023-12-18 06:33:58 +0530

    Added deactive forgot password service

--- services/forgotPassword.js
+++ services/forgotPassword.js
@@ -3,6 +3,7 @@ const { v4 } = require('uuid');
 const ForgotPassword = require('../models/forgotPassword');
 
 
+
 exports.activeForgotPassword = async options => {
     try {
         const { userId } = options;
@@ -10,9 +11,28 @@ exports.activeForgotPassword = async options => {
         const { id } = await ForgotPassword.create({
             id: v4(),
             userId,
+            isActive: true,
         });
 
         return id;
+
+    } catch (error) {
+        throw error
+    }
+}
+
+
+exports.deactiveForgotPassword = async options => {
+    try {
+        const { id } = options;
+
+        const forgotPassword = await ForgotPassword.findByPk(id);
+        
+        forgotPassword.isActive = false;
+
+        forgotPassword.save();
+
+        return;
 
     } catch (error) {
         throw error
