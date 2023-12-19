0a013ab06dc8b7838d8204ad81dacb6f572a7911
Shreyash Daundkar <productiveappa@gmail.com>
2023-12-19 12:07:41 +0530

    Installed and added sib service for sending forget password mails.

---
+++ services/sib.js
@@ -0,0 +1,34 @@
+const Sib = require('sib-api-v3-sdk');
+
+
+
+exports.sendMail = async options => {
+    try {
+        const { email, forgetPasswordId } = options; 
+    
+        const client = Sib.ApiClient.instance;
+        const apiKey = client.authentications['api-key'];
+        apiKey.apiKey = process.env.SIB_API;
+        
+        const transEmailApi = new Sib.TransactionalEmailsApi();
+    
+        const sender = {
+            email: process.env.SIB_SENDER_EMAIL,
+            name: process.env.SIB_SENDER_NAME,
+        }
+    
+        const receivers = [ { email } ];
+
+        await transEmailApi.sendTransacEmail({
+            sender,
+            to: receivers,
+            subject: 'Forget Password',
+            textContent: `http://${req.headers.host}/reset-password.html?id=${forgetPasswordId}`,
+        });  
+
+        return;
+    } catch (error) {
+        throw error; 
+    }
+
+}
